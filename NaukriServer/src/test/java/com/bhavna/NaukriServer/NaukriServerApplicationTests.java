27715f2fe8d2482ad7447c05c05a296f16f88375
Sravankumar-Doddi <Sravan.Doddi@bhavnacorp.com>
2022-03-15 13:16:11 +0530

    Server

---
+++ NaukriServer/src/test/java/com/bhavna/NaukriServer/NaukriServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavna.NaukriServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukriServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
